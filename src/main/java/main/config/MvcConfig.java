85797e8ed2d2c37a04b8f61eb710b6221b838fd4
Light77-Anton <antonbauman8@gmail.com>
2022-08-07 12:10:36 +0300

    stage 6 try 83

--- src/main/java/main/config/MvcConfig.java
+++ src/main/java/main/config/MvcConfig.java
@@ -5,7 +5,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
@@ -13,11 +12,11 @@ public class MvcConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/upload/**").addResourceLocations("file:/upload/");
         registry.addResourceHandler("/avatars/**").addResourceLocations("file:/avatars/");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
-        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
-        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
-        registry.addResourceHandler("/favicon.ico").addResourceLocations("file:/");
-        registry.addResourceHandler("/default-1.png").addResourceLocations("file:/");
+        //registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        //registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        //registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
+        //registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+        //registry.addResourceHandler("/favicon.ico").addResourceLocations("file:/");
+        //registry.addResourceHandler("/default-1.png").addResourceLocations("file:/");
     }
 }
